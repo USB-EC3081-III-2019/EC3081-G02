50eea39481712f3a55a9fe08ed822cc6fa782d5a
GEnma29 <53823068+GEnma29@users.noreply.github.com>
2019-12-09 19:25:47 -0400

    Codigo RELAY

---
+++ firmware/RELAY.c
@@ -0,0 +1,547 @@
+#include <xc.h>
+#pragma config FOSC = XT        // Oscillator Selection bits (XT oscillator)
+#pragma config WDTE = OFF       // Watchdog Timer Enable bit (WDT disabled)
+#pragma config PWRTE = OFF      // Power-up Timer Enable bit (PWRT disabled)
+#pragma config CP = OFF         // FLASH Program Memory Code Protection bits (Code protection off)
+#pragma config BOREN = OFF      // Brown-out Reset Enable bit (BOR disabled)
+#pragma config LVP = OFF        // Low Voltage In-Circuit Serial Programming Enable bit (RB3 is digital I/O, HV on MCLR must be used for programming)
+#pragma config CPD = OFF        // Data EE Memory Code Protection (Code Protection off)
+#pragma config WRT = ON         // FLASH Program Memory Write Enable (Unprotected program memory may be written to by EECON control)
+
+#define _XTAL_FREQ 4000000
+
+#define LCD_RS PORTCbits.RC1
+#define LCD_E PORTCbits.RC0
+#define LCD_DATO PORTD
+
+void LCD_Escribir(char dato);
+void LCD_Cmd(char dato);
+void Presencia_salir();
+void LCD_Configuracion_Inicial(void);
+void PIC_Configuracion_Inicial(void);
+void LCD_Escribir_Cadena(char str[]);
+void LCD_Cursor(int Horizontal, int Vertical);
+void LCD_Display(int Tam);
+int LCD_Contar(char c[]);
+void ClosePersianas();
+void Intro();
+void Delay_2s();
+void AperturaDepercians();
+void Timer0Delay(void);
+void Retardo_2segundos();
+void Visualisar_Config();
+void Temperatura_Humedad();
+void Retardo_1segundos();
+void Humedad();
+void OpenPersianas();
+void Temperatura();
+void Luz_Natural();
+void Luz_Na_Ar();
+void Luz_02_Artificial();
+void Luz_03_Artificial_Manual();
+void Luz_03_Artificial_Automatica();
+void AbrirPercianas();
+void AperturaHig();
+void Aperturamedia();
+void Aperturalow();
+void RELAY_01();
+void Luz_05_Artificial_Manual();
+unsigned char n;
+unsigned int Status,Variable,j,l,g;
+unsigned char n = ' ';
+unsigned int a = 0, Q = 0;
+unsigned char selector1,selector2,selector3,selector4,selector5;
+
+void main(void) {
+    char aux0[] = "0";
+    int selector1 = 0;
+    a = 0;
+
+    PIC_Configuracion_Inicial();
+    LCD_Configuracion_Inicial();
+    PORTAbits.RA1 = 0; // Configuracion en modo automatico
+
+    //RBIE = 0;
+    Intro();
+    Retardo_2segundos();
+    RELAY_01();
+    Retardo_2segundos();
+    while(1){
+    switch(n){
+        if((n == '1')&&(a == 0)){
+           n = '1'; 
+        }
+        if((n == '2')&&( a == 0)){
+            n = '2';
+        }
+        case '1':
+             n = ' ';
+             a = 1;
+             PORTEbits.RE0 = 1;
+            Luz_03_Artificial_Manual(); 
+            Retardo_2segundos();
+           
+            
+            break;
+        
+        case '2':
+             n = ' ';
+             selector2 = '0';
+             PORTEbits.RE0 = 0;
+            Luz_05_Artificial_Manual();
+            Retardo_2segundos();
+            
+             
+}
+}
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////Libreria INTERFACE///////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////Libreria LCD/////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////
+void LCD_Escribir(char dato){
+    LCD_RS = 1;
+    LCD_DATO = dato;
+    LCD_E = 1;
+    LCD_E = 0;
+    __delay_us(40);
+}
+
+void LCD_Cmd(char dato) {
+    LCD_RS = 0;
+    LCD_DATO = dato;
+    LCD_E = 1;
+    LCD_E = 0;
+    if (dato <= 3) {
+        __delay_ms(2);
+    } else {
+        __delay_us(40);
+    }
+}
+
+void LCD_Configuracion_Inicial(void) {
+    LCD_E = 0;
+    __delay_ms(15);
+    LCD_Cmd(0x38);
+    __delay_ms(5);
+    LCD_Cmd(0x38);
+    __delay_ms(1);
+    LCD_Cmd(0x38);
+    LCD_Cmd(0x38);
+    LCD_Cmd(0x0C);
+    LCD_Cmd(0x01); //Display clear
+    LCD_Cmd(0x06); //incrementar el cursor
+
+    LCD_Cmd(0x0C); //DISPLAY ON/ CURSOR OFF /NO BLINKING
+
+}
+
+void LCD_Escribir_Cadena(char str[]) {
+    int i = 0;
+    while (str[i] != '\0' && i < 1000) {
+        LCD_Escribir(str[i]);
+        i++;
+    }
+}
+
+void LCD_Cursor(int h, int v) {
+    int i;
+
+    if (v == 1) {
+        for (i = 0; i < h; i++) {
+            __delay_us(40);
+            LCD_Cmd(0x14);
+        }
+    }
+
+    if (v == 2) {
+        h = h + 64;
+        for (i = 0; i < h; i++) {
+            __delay_us(40);
+            LCD_Cmd(0x14);
+        }
+    }
+
+}
+
+int LCD_Contar(char c[]) {
+    int i = 0;
+    while (c[i] != '\0' && i < 1000) {
+        i++;
+    }
+    return i;
+}
+
+void LCD_Display(int Tam) {
+    int aux1, i;
+    __delay_ms(2);
+    LCD_Cmd(0x02);
+
+    __delay_ms(1000);
+    if (Tam > 16 && Tam < 64) {
+        aux1 = Tam - 16;
+        for (i = -1; i < aux1; i++) {
+            __delay_ms(300);
+            LCD_Cmd(0x1B);
+        }
+    }
+    __delay_ms(2);
+    LCD_Cmd(0x02);
+    __delay_ms(2);
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////Libreria LCD/////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+void PIC_Configuracion_Inicial(void) {
+    TRISD = 0;
+    TRISCbits.TRISC0 = 0;
+    TRISCbits.TRISC1 = 0;
+    TRISA = 0;
+    TRISE = 0;
+    TRISB = 0b11110000;
+    ADCON0 = 0b00000000; //Configuracion del registro ADCON0
+    /*INICIALIZACIÓN DE INTERRUPCIONES*/
+    INTCON = 0; //limpiar registro INTCON
+    INTCONbits.GIE = 1; //Hab interrupciones Globales
+    PORTB = 0;
+    RBIF = 0;
+    INTCONbits.RBIE = 1; //Port b
+
+    OPTION_REGbits.nRBPU = 0;
+}
+
+void __interrupt() Interrupcion(void) {
+    if (RBIF) //Si hay cambio de estado en PORTB
+    {
+        char a = 0, b = 1, c = 1, i = 1, k = 1;
+        while (k == 1) {
+            PORTBbits.RB3 = a;
+            PORTBbits.RB2 = b;
+            PORTBbits.RB1 = c;
+
+
+            if ((PORTBbits.RB7 == 0)&&(PORTBbits.RB3 == 0)) //Código de atención de la interrupción
+            {
+                //LCD_Comando(0x01); //Display clear
+                __delay_ms(2);
+                //LCD_Escribir('*');
+                n = '*';
+                k = 0;
+                __delay_ms(50);
+
+            }
+            if ((PORTBbits.RB7 == 0)&&(PORTBbits.RB2 == 0))//Código de atención de la interrupción
+            {
+                //LCD_Comando(0x01); //Display clear
+                __delay_ms(2);
+                //LCD_Escribir('0');
+                n = '0';
+                k = 0;
+                __delay_ms(50);
+            }
+            if ((PORTBbits.RB7 == 0)&&(PORTBbits.RB1 == 0))//Código de atención de la interrupción
+            {
+                __delay_ms(2);
+                //LCD_Escribir('#');
+                n = '#';
+                k = 0;
+                __delay_ms(50);
+            }
+            if ((PORTBbits.RB6 == 0)&&(PORTBbits.RB3 == 0)) //Código de atención de la interrupción
+            {
+                //LCD_Comando(0x01); //Display clear
+                __delay_ms(2);
+                //LCD_Escribir('7');
+                n = '7';
+                k = 0;
+                __delay_ms(50);
+
+            }
+            if ((PORTBbits.RB6 == 0)&&(PORTBbits.RB2 == 0))//Código de atención de la interrupción
+            {
+                //LCD_Comando(0x01); //Display clear
+                __delay_ms(2);
+                //LCD_Escribir('8');
+                n = '8';
+                k = 0;
+                __delay_ms(50);
+            }
+            if ((PORTBbits.RB6 == 0)&&(PORTBbits.RB1 == 0))//Código de atención de la interrupción
+            {
+                //LCD_Comando(0x01); //Display clear
+                __delay_ms(2);
+                //LCD_Escribir('9');
+                n = '9';
+                k = 0;
+                __delay_ms(50);
+            }
+            //
+            if ((PORTBbits.RB5 == 0)&&(PORTBbits.RB3 == 0)) //Código de atención de la interrupción
+            {
+                //LCD_Comando(0x01); //Display clear
+                __delay_ms(2);
+                //LCD_Escribir('4');
+                n = '4';
+                k = 0;
+                __delay_ms(50);
+            }
+            if ((PORTBbits.RB5 == 0)&&(PORTBbits.RB2 == 0))//Código de atención de la interrupción
+            {
+                //LCD_Comando(0x01); //Display clear
+                __delay_ms(2);
+                //LCD_Escribir('5');
+                n = '5';
+                k = 0;
+                __delay_ms(50);
+            }
+            if ((PORTBbits.RB5 == 0)&&(PORTBbits.RB1 == 0))//Código de atención de la interrupción
+            {
+                //LCD_Comando(0x01); //Display clear
+                __delay_ms(2);
+                //LCD_Escribir('6');
+                n = '6';
+                k = 0;
+                __delay_ms(50);
+            }
+            //
+            if ((PORTBbits.RB4 == 0)&&(PORTBbits.RB3 == 0)) //Código de atención de la interrupción
+            {
+                //LCD_Comando(0x01); //Display clear
+                __delay_ms(2);
+                //LCD_Escribir('1');
+                n = '1';
+                k = 0;
+                __delay_ms(50);
+            }
+            if ((PORTBbits.RB4 == 0)&&(PORTBbits.RB2 == 0))//Código de atención de la interrupción
+            {
+                //LCD_Comando(0x01); //Display clear
+                __delay_ms(2);
+                //LCD_Escribir('2');
+                n = '2';
+                k = 0;
+                __delay_ms(50);
+            }
+            if ((PORTBbits.RB4 == 0)&&(PORTBbits.RB1 == 0))//Código de atención de la interrupción
+            {
+                //LCD_Comando(0x01); //Display clear
+                __delay_ms(2);
+                //LCD_Escribir('3');
+                n = '3';
+                k = 0;
+                __delay_ms(50);
+            }
+
+            if (i == 1) {
+                a = 1;
+                b = 0;
+                c = 1;
+            }
+            if (i == 2) {
+                a = 1;
+                b = 1;
+                c = 0;
+                //i=-1;
+            }
+            if (i == 3) {
+                k = 0;
+                i = -1;
+            }
+            i++;
+        }
+        PORTB = 0;
+        RBIF = 0; //Desactivar bandera...
+
+        //INTCONbits.RBIF==0;
+    }
+
+}
+void Intro(){
+ 
+    LCD_Cursor(1,1);
+    LCD_Escribir_Cadena("Welcome to");
+    LCD_Cursor(0,0);
+    LCD_Cursor(-6,2);
+    LCD_Escribir_Cadena("Smartvite");
+}
+void Visualisar_Config(){
+    LCD_Cmd(0x01); // Limpiar Display
+    LCD_Cursor(1,1);
+    LCD_Escribir_Cadena("1.visualizar");
+    LCD_Cursor(0,0);
+    LCD_Cursor(-12,2);
+    LCD_Escribir_Cadena("2.configurar");
+}
+void Temperatura_Humedad(){
+     LCD_Cmd(0x01); // Limpiar Display
+    LCD_Cursor(1,1);
+    LCD_Escribir_Cadena("1.Temperatura");
+    LCD_Cursor(-13,2);
+    LCD_Escribir_Cadena("2.Humedad");
+}
+void Luz_Na_Ar(){
+     LCD_Cmd(0x01); // Limpiar Display
+    LCD_Cursor(1,1);
+    LCD_Escribir_Cadena("1.Luz Natural");
+    LCD_Cursor(-14,2);
+    LCD_Escribir_Cadena("2.Luz Artificial");
+   
+}
+void AbrirPercianas(){
+    LCD_Cmd(0x01); // Limpiar Display
+    LCD_Escribir_Cadena("1.OPEN");
+    LCD_Cursor(-15,2);
+    LCD_Escribir_Cadena("2.CLOSE");
+
+}
+void Presencia(){
+    LCD_Cmd(0x01); // Limpiar Display
+    LCD_Cursor(1,1);
+    LCD_Escribir_Cadena("3.Presencia");
+    LCD_Cursor(-7,2);
+    LCD_Escribir_Cadena("4.Salir");
+}
+void Luz_02_Artificial(){
+    LCD_Cmd(0x01); // Limpiar Display
+    LCD_Cursor(1,1);
+    LCD_Escribir_Cadena("1.Automatica");
+    LCD_Cursor(-8,2);
+    LCD_Escribir_Cadena("2.manual");
+}
+void Luz_03_Artificial_Manual(){
+    LCD_Cmd(0x01); // Limpiar Display
+    LCD_Cursor(1,1);
+    LCD_Escribir_Cadena("Las luces han sido ");
+     LCD_Cursor(-10,2);
+    LCD_Escribir_Cadena("encendidas");
+}
+void Luz_05_Artificial_Manual(){
+    LCD_Cmd(0x01); // Limpiar Display
+    LCD_Cursor(1,1);
+    LCD_Escribir_Cadena("Las luces han sido ");
+     LCD_Cursor(-10,2);
+    LCD_Escribir_Cadena("Apagadas");
+}
+void Luz_03_Artificial_Automatica(){
+    LCD_Cmd(0x01); // Limpiar Display
+    LCD_Cursor(1,1);
+    LCD_Escribir_Cadena("Las luces seran ");
+     LCD_Cursor(-19,2);
+    LCD_Escribir_Cadena("encendidas a las 6 pm");
+}
+void Temperatura(){
+    LCD_Cmd(0x01); // Limpiar Display
+    LCD_Cursor(1,1);
+    LCD_Escribir_Cadena("La Temperatura ");
+    LCD_Cursor(-12,2);
+    LCD_Escribir_Cadena("26 C");
+}
+void Humedad(){
+    LCD_Cmd(0x01); // Limpiar Display
+    LCD_Cursor(1,1);
+    LCD_Escribir_Cadena("La Humedad es:");
+    LCD_Cursor(-10,2);
+    LCD_Escribir_Cadena("68%");
+
+}
+void Luz_Natural(){
+    LCD_Cmd(0x01); // Limpiar Display
+    LCD_Cursor(0,1);
+    LCD_Escribir_Cadena("1.abrir");
+    LCD_Cursor(-7,2);
+    LCD_Escribir_Cadena("2.cerrar");
+}
+void Presencia_salir(){
+LCD_Cmd(0x01); // Limpiar Display
+    LCD_Cursor(1,1);
+    LCD_Escribir_Cadena("3.Precensia");
+    LCD_Cursor(-11,2);
+    LCD_Escribir_Cadena("4.Salir");
+
+}
+void Retardo_2segundos(){
+    int i;
+    for(i=0; i<20;i++){
+        __delay_ms(100);
+    }
+}
+void Retardo_1segundos(){
+    int i;
+    for(i=0; i<10;i++){
+        __delay_ms(100);
+    }
+}
+void ClosePersianas(){
+    LCD_Cmd(0x01); // Limpiar Display
+    LCD_Cursor(1,1);
+    LCD_Escribir_Cadena("Las percianas han");
+    LCD_Cursor(-13,2);
+    LCD_Escribir_Cadena("sido cerradas");
+}
+void OpenPersianas(){
+    LCD_Cmd(0x01); // Limpiar Display
+    LCD_Cursor(1,1);
+    LCD_Escribir_Cadena("Las percianas han");
+    LCD_Cursor(-13,2);
+    LCD_Escribir_Cadena("sido abiertas");
+}
+void AperturaDepercians(){
+
+
+}
+void Delay_2s(){
+    int i;
+    for(i = 0; i < 200; i++){//Toggle Output
+        if(n != 0){
+            i=200; //Delay
+        }       
+    }
+
+
+}
+void Aperturalow(){
+       LCD_Cmd(0x01); // Limpiar Display
+    LCD_Cursor(1,1);
+    LCD_Escribir_Cadena("Las apertura es de ");
+    LCD_Cursor(-13,2);
+    LCD_Escribir_Cadena("23");
+}
+void Aperturamedia(){
+       LCD_Cmd(0x01); // Limpiar Display
+    LCD_Cursor(1,1);
+    LCD_Escribir_Cadena("Las apertura es de ");
+    LCD_Cursor(-13,2);
+    LCD_Escribir_Cadena("56");
+}
+void AperturaHig(){
+       LCD_Cmd(0x01); // Limpiar Display
+    LCD_Cursor(1,1);
+    LCD_Escribir_Cadena("Las apertura es de ");
+    LCD_Cursor(-13,2);
+    LCD_Escribir_Cadena("76");
+}
+void RELAY_01(){
+    LCD_Cmd(0x01); // Limpiar Display
+    LCD_Cursor(1,1);
+    LCD_Escribir_Cadena("1.Activar");
+    LCD_Cursor(-8,2);
+    LCD_Escribir_Cadena("2.Desactivar");
+}
+void Timer0Delay(void){     //10ms delay
+    T1CON=0x01;             //Timer-1 16-bit mode Prescaler 1:4
+    TMR1H=0x30;             //Count Hight Byte
+    TMR1L=0xD4;             //Count Low Byte
+   
+    T1CONbits.TMR1ON=1;              //Run timer
+    while(INTCONbits.TMR0IF==0);     //Wait for flag to over flow
+    T1CONbits.TMR1ON=0;              //Switch off timer
+    INTCONbits.TMR0IF=0;             //Clear Interrupt
+   
+}
